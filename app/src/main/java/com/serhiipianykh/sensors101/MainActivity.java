25cd132cd431fe6408ab28d2be555d053fde0713
serhiipianykh <serhiipianykh@gmail.com>
2017-02-27 15:49:13 -0500

    list all sensors

--- app/src/main/java/com/serhiipianykh/sensors101/MainActivity.java
+++ app/src/main/java/com/serhiipianykh/sensors101/MainActivity.java
@@ -1,13 +1,35 @@
 package com.serhiipianykh.sensors101;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private TextView sensorsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorsList = (TextView) findViewById(R.id.sensors_list);
+
+        //getting reference to device sensor services
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        //list of all device sensors
+        List<Sensor> deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        //all items to textview
+        for (int i = 0; i < deviceSensors.size(); i++) {
+            sensorsList.append(deviceSensors.get(i).getName() + "\n");
+        }
     }
 }
